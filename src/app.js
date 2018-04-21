103e473a748956664b6b7ae5db19e14fd5a51c62
Your Name <nazik_111@mail.ru>
2018-04-21 12:59:47 +0500

    Create categories

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import { http } from './http';
 // Search input
 const searchWord = document.getElementById("search_word");
 
+// Array of categories
+const categories = [];
+
 // Search input event listener
 searchWord.addEventListener('keyup', (e) => {
     // Get input text
@@ -21,11 +24,28 @@ function getWords(input) {
 
     http.get('http://localhost:3000/words')
         .then(data => data.forEach(function (word) {
-            let output = '';
+            // Initialize words_output 
+            let words_output = '';
+
+            // Initialize categories_output
+            let categories_output = '';
+
+            // Check if a searched word in json matches input 
             const matchedWord = word.word_uyghur_cyrilic === input;
+
+            // Fetch all related words
             const related_words = word.related_words;
+
+            // Cache related words container
             let related_words_container = document.querySelector('.related_words_container');
+
+            // Cache related words categories container
+            let related_words_categories_container = document.querySelector('.categories_container');
+
+
             if (matchedWord) {
+
+                // Append found words to container
                 document.querySelector('.container').innerHTML = `
                 <br>
                 <span>${word.word_uyghur_cyrilic} - ${word.translation_russian}</span>
@@ -33,19 +53,34 @@ function getWords(input) {
 
                 // Object of related words
                 Object.keys(related_words).forEach(function (word) {
-                    //console.log(related_words[word].word_uyghur_cyrilic);
-                    //console.log(related_words[word].translation_russian);
-                    output +=`<div>${related_words[word].word_uyghur_cyrilic} - ${related_words[word].translation_russian};</div>`
+
+                    // Populate words output with divs
+                    words_output += `<div class="${related_words[word].category}">${related_words[word].word_uyghur_cyrilic} - ${related_words[word].translation_russian};</div>`
+
+                    // Push categories array with categories from JSON
+                    categories.push(related_words[word].category);
+
                 });
 
-                //console.log(typeof(output));
-                related_words_container.innerHTML = output;
+                const _categories = getCategories(categories);
+
+                _categories.forEach(function (category) {
+                    categories_output += `<a href="${category}">${category}</a>`
+                })
+
+                related_words_container.innerHTML = words_output;
+                //console.table(categories);
+
+
+                related_words_categories_container.innerHTML = categories_output
             }
         }))
         .catch(err => console.log(err));
 
-        function getCategories(category, className) {
-            
-        }
+    // Function responsible for creating a unique array
+    function getCategories(categories) {
+        var uniqueArray = Array.from(new Set(categories));
+        return uniqueArray;
+    }
 }
 
